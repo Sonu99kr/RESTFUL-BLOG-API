093408b6a076feff1cb3136d56674208d7248ad1
Sonu99kr <krsonu.work@gmail.com>
2025-07-15 13:00:00 +0530

    adding frontend in ejs format

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -5,6 +5,18 @@ const {
 } = require("../controllers/userControllers");
 const router = express.Router();
 
+router.get("/", (req, res) => {
+  return res.render("home");
+});
+
+router.get("/login", (req, res) => {
+  return res.render("login");
+});
+
+router.get("/register", (req, res) => {
+  return res.render("register");
+});
+
 router.post("/login", handleUserLogin);
 router.post("/register", handleUserRegister);
 
--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const { connectToDB } = require("./config/database");
 require("dotenv").config();
+const userRoutes = require("./routes/userRoutes");
+const path = require("path");
 
 const app = express();
 const PORT = process.env.PORT;
 
 connectToDB();
 
-app.use(express.json());
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
 
-app.get("/", (req, res) => {
-  res.send("API is running");
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use("/api/users", userRoutes);
+app.use("/", userRoutes);
 
 app.listen(PORT, () => {
   console.log(`server started at PORT:${PORT}`);
