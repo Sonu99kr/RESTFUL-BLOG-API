884197729548dbc120da63227e68760d65f0980b
Sonu99kr <krsonu.work@gmail.com>
2025-08-03 18:51:53 +0530

    navbar added

--- frontend/Blog-Frontend/src/Components/Pages/HomePage.jsx
+++ frontend/Blog-Frontend/src/Components/Pages/HomePage.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Navbar from "./Navbar";
 
 function HomePage() {
   return (
-    <div>
-      <p>this is home page</p>
-    </div>
+    <>
+      <Navbar />
+    </>
   );
 }
 
